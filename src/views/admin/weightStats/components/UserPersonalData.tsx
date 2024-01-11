d1e1fcc676506a192cbd4c206ccea5cc38834e8c
LackOnUsernameIdeas <kaloyan.kostadinov2007@abv.bg>
2024-01-11 16:28:17 +0200

    components of weightstats

--- src/views/admin/weightStats/components/UserPersonalData.tsx
+++ src/views/admin/weightStats/components/UserPersonalData.tsx
@@ -1,5 +1,5 @@
 // Import necessary dependencies
-import React from 'react';
+import React from "react";
 import {
   Box,
   Text,
@@ -7,188 +7,209 @@ import {
   Radio,
   Input,
   Button,
-  Stack,
-} from '@chakra-ui/react';
+  Stack
+} from "@chakra-ui/react";
 import Card from "components/card/Card";
-import { UserData, HealthInfo, BodyMass } from '../../../../types/weightStats';
-
+import { UserData, HealthInfo, BodyMass } from "../../../../types/weightStats";
 
 interface UserPersonalDataProps {
-    userData: UserData;
-    handleInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-    handleRadioChange: (key: string, value: string) => void;
-    generateStats: () => void;
+  userData: UserData;
+  handleInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  handleRadioChange: (key: string, value: string) => void;
+  generateStats: () => void;
 }
 
-const userDataPropertiesTranslated = ['пол', 'ръст', 'възраст', 'тегло', 'обиколка на врат', 'обиколка на талия', 'oбиколка на бедро', 'цел'];
+const userDataPropertiesTranslated = [
+  "пол",
+  "ръст",
+  "възраст",
+  "тегло",
+  "обиколка на врат",
+  "обиколка на талия",
+  "oбиколка на бедро",
+  "цел"
+];
 
 // Define the UserForm component
 const UserPersonalData: React.FC<UserPersonalDataProps> = ({
-    userData,
-    handleInputChange,
-    handleRadioChange,
-    generateStats
+  userData,
+  handleInputChange,
+  handleRadioChange,
+  generateStats
 }) => {
-    
-    const isUserDataValid = () => {
-        // Validation logic goes here
-        // Example: Numerical values should be greater than zero
-        return (
-        userData.height > 0 &&
-        userData.age > 0 &&
-        userData.weight > 0 &&
-        userData.neck > 0 &&
-        userData.waist > 0 &&
-        userData.hip > 0
-        );
-    };
-
-    const handleSubmit = () => {
-        if (isUserDataValid()) {
-            generateStats();
-        } else {
-        // Handle invalid data, you can show an error message or take any other appropriate action
-            alert('Please provide valid values for all fields before submitting.');
-        // Or you can use a state to manage and display an error message in your UI
-        // setError('Please provide valid values for all fields before submitting.');
-        }
-    };
-
+  const isUserDataValid = () => {
+    // Validation logic goes here
+    // Example: Numerical values should be greater than zero
     return (
-        <Card
-            overflowX={{ base: 'scroll', md: 'hidden' }}
-            p="20px"
-            alignItems="center"
-            flexDirection="column"
-            w="100%"
-            mb="20px"
-            maxW="100%"
-        >
-            <Box gap="10px" mb="20px">
-            {Object.entries(userData).map(([key, value], index) => (
-                <label key={key}>
-                {userDataPropertiesTranslated[index].charAt(0).toUpperCase() + userDataPropertiesTranslated[index].slice(1)}:
-                {typeof value === "number" ? (
-                    value !== 0 ?
-                        (<Input
-                            variant='auth'
-                            type="number"
-                            name={key}
-                            value={value || ''}
-                            placeholder={"Въведете " + userDataPropertiesTranslated[index]}
-                            autoComplete="on"
-                            onChange={(e) => handleInputChange(e)}
-                        />)     
-                    :
-                        (<Input
-                            variant='auth'
-                            type="number"
-                            name={key}
-                            value={''}
-                            placeholder={"Въведете " + userDataPropertiesTranslated[index]}
-                            autoComplete="on"
-                            onChange={(e) => handleInputChange(e)}
-                        />)
-                ) : key === "gender" ? (
-                <Stack direction="row">
-                    <Radio
-                    value="male"
-                    onChange={() => handleRadioChange(key, "male")}
-                    isChecked={value === "male"}
-                    >
-                    Мъж
-                    </Radio>
-                    <Radio
-                    value="female"
-                    onChange={() => handleRadioChange(key, "female")}
-                    isChecked={value === "female"}
-                    >
-                    Жена
-                    </Radio>
-                </Stack>
-                ) : key === "goal" ? (
-                <Stack direction="row" alignContent="center" justify="center">
-                    <SimpleGrid columns={{ base: 3, md: 1, lg: 7 }} spacing="10px" alignItems="center" mb="10px">
-                    <Radio
-                        value="maintain"
-                        onChange={() => handleRadioChange(key, "maintain")}
-                        isChecked={value === "maintain"}
-                        fontSize={{ base: "sm", md: "md", lg: "lg" }}  // Adjust the sizes based on your design
-                        ml={{ base: "3%", md: "3%", lg: "3%" }}
-                    >
-                        Запази тегло
-                    </Radio>
-                    <Radio
-                        value="mildlose"
-                        onChange={() => handleRadioChange(key, "mildlose")}
-                        isChecked={value === "mildlose"}
-                        fontSize={{ base: "sm", md: "md", lg: "lg" }}  // Adjust the sizes based on your design
-                        mr={{ base: "3%", md: "3%", lg: "3%" }}
-                    >
-                        Леко сваляне на тегло
-                    </Radio>
-                    <Radio
-                        value="weightlose"
-                        onChange={() => handleRadioChange(key, "weightlose")}
-                        isChecked={value === "weightlose"}
-                        fontSize={{ base: "sm", md: "md", lg: "lg" }}  // Adjust the sizes based on your design
-                        mr={{ base: "3%", md: "3%", lg: "3%" }}
-                    >
-                        Сваляне на тегло
-                    </Radio>
-                    <Radio
-                        value="extremelose"
-                        onChange={() => handleRadioChange(key, "extremelose")}
-                        isChecked={value === "extremelose"}
-                        fontSize={{ base: "sm", md: "md", lg: "lg" }}  // Adjust the sizes based on your design
-                        mr={{ base: "3%", md: "3%", lg: "3%" }}
-                    >
-                        Екстремно сваляне на тегло
-                    </Radio>
-                    <Radio
-                        value="mildgain"
-                        onChange={() => handleRadioChange(key, "mildgain")}
-                        isChecked={value === "mildgain"}
-                        fontSize={{ base: "sm", md: "md", lg: "lg" }}  // Adjust the sizes based on your design
-                        mr={{ base: "3%", md: "3%", lg: "3%" }}
-                    >
-                        Леко качване на тегло
-                    </Radio>
-                    <Radio
-                        value="weightgain"
-                        onChange={() => handleRadioChange(key, "weightgain")}
-                        isChecked={value === "weightgain"}
-                        fontSize={{ base: "sm", md: "md", lg: "lg" }}  // Adjust the sizes based on your design
-                        mr={{ base: "3%", md: "3%", lg: "3%" }}
-                    >
-                        Качване на тегло
-                    </Radio>
-                    <Radio
-                        value="extremegain"
-                        onChange={() => handleRadioChange(key, "extremegain")}
-                        isChecked={value === "extremegain"}
-                        fontSize={{ base: "sm", md: "md", lg: "lg" }}  // Adjust the sizes based on your designfontSize={{ base: "sm", md: "md", lg: "lg" }}  // Adjust the sizes based on your design
-                        mr={{ base: "3%", md: "3%", lg: "3%" }}
-                    >
-                        Екстремно качване на тегло
-                    </Radio>
-                    </SimpleGrid>
-                </Stack>
-                ) : (
-                    <Input
-                    type="radio"
-                    id={value}
-                    name="goal"
-                    value={value}
-                    onChange={(e) => handleInputChange(e)}
-                    />
-                )}
-                </label>
-            ))}
-            <Button type="submit" onClick={handleSubmit}>Изпрати</Button>
-            </Box>
-        </Card>
+      userData.height > 0 &&
+      userData.age > 0 &&
+      userData.weight > 0 &&
+      userData.neck > 0 &&
+      userData.waist > 0 &&
+      userData.hip > 0
     );
+  };
+
+  const handleSubmit = () => {
+    if (isUserDataValid()) {
+      generateStats();
+    } else {
+      // Handle invalid data, you can show an error message or take any other appropriate action
+      alert("Please provide valid values for all fields before submitting.");
+      // Or you can use a state to manage and display an error message in your UI
+      // setError('Please provide valid values for all fields before submitting.');
+    }
+  };
+
+  return (
+    <Card
+      overflowX={{ base: "scroll", md: "hidden" }}
+      p="20px"
+      alignItems="center"
+      flexDirection="column"
+      w="100%"
+      mb="20px"
+      maxW="100%"
+    >
+      <Box gap="10px" mb="20px">
+        {Object.entries(userData).map(([key, value], index) => (
+          <label key={key}>
+            {userDataPropertiesTranslated[index].charAt(0).toUpperCase() +
+              userDataPropertiesTranslated[index].slice(1)}
+            :
+            {typeof value === "number" ? (
+              value !== 0 ? (
+                <Input
+                  variant="auth"
+                  type="number"
+                  name={key}
+                  value={value || ""}
+                  placeholder={
+                    "Въведете " + userDataPropertiesTranslated[index]
+                  }
+                  autoComplete="on"
+                  onChange={(e) => handleInputChange(e)}
+                />
+              ) : (
+                <Input
+                  variant="auth"
+                  type="number"
+                  name={key}
+                  value={""}
+                  placeholder={
+                    "Въведете " + userDataPropertiesTranslated[index]
+                  }
+                  autoComplete="on"
+                  onChange={(e) => handleInputChange(e)}
+                />
+              )
+            ) : key === "gender" ? (
+              <Stack direction="row">
+                <Radio
+                  value="male"
+                  onChange={() => handleRadioChange(key, "male")}
+                  isChecked={value === "male"}
+                >
+                  Мъж
+                </Radio>
+                <Radio
+                  value="female"
+                  onChange={() => handleRadioChange(key, "female")}
+                  isChecked={value === "female"}
+                >
+                  Жена
+                </Radio>
+              </Stack>
+            ) : key === "goal" ? (
+              <Stack direction="row" alignContent="center" justify="center">
+                <SimpleGrid
+                  columns={{ base: 3, md: 1, lg: 7 }}
+                  spacing="10px"
+                  alignItems="center"
+                  mb="10px"
+                >
+                  <Radio
+                    value="maintain"
+                    onChange={() => handleRadioChange(key, "maintain")}
+                    isChecked={value === "maintain"}
+                    fontSize={{ base: "sm", md: "md", lg: "lg" }} // Adjust the sizes based on your design
+                    ml={{ base: "3%", md: "3%", lg: "3%" }}
+                  >
+                    Запази тегло
+                  </Radio>
+                  <Radio
+                    value="mildlose"
+                    onChange={() => handleRadioChange(key, "mildlose")}
+                    isChecked={value === "mildlose"}
+                    fontSize={{ base: "sm", md: "md", lg: "lg" }} // Adjust the sizes based on your design
+                    mr={{ base: "3%", md: "3%", lg: "3%" }}
+                  >
+                    Леко сваляне на тегло
+                  </Radio>
+                  <Radio
+                    value="weightlose"
+                    onChange={() => handleRadioChange(key, "weightlose")}
+                    isChecked={value === "weightlose"}
+                    fontSize={{ base: "sm", md: "md", lg: "lg" }} // Adjust the sizes based on your design
+                    mr={{ base: "3%", md: "3%", lg: "3%" }}
+                  >
+                    Сваляне на тегло
+                  </Radio>
+                  <Radio
+                    value="extremelose"
+                    onChange={() => handleRadioChange(key, "extremelose")}
+                    isChecked={value === "extremelose"}
+                    fontSize={{ base: "sm", md: "md", lg: "lg" }} // Adjust the sizes based on your design
+                    mr={{ base: "3%", md: "3%", lg: "3%" }}
+                  >
+                    Екстремно сваляне на тегло
+                  </Radio>
+                  <Radio
+                    value="mildgain"
+                    onChange={() => handleRadioChange(key, "mildgain")}
+                    isChecked={value === "mildgain"}
+                    fontSize={{ base: "sm", md: "md", lg: "lg" }} // Adjust the sizes based on your design
+                    mr={{ base: "3%", md: "3%", lg: "3%" }}
+                  >
+                    Леко качване на тегло
+                  </Radio>
+                  <Radio
+                    value="weightgain"
+                    onChange={() => handleRadioChange(key, "weightgain")}
+                    isChecked={value === "weightgain"}
+                    fontSize={{ base: "sm", md: "md", lg: "lg" }} // Adjust the sizes based on your design
+                    mr={{ base: "3%", md: "3%", lg: "3%" }}
+                  >
+                    Качване на тегло
+                  </Radio>
+                  <Radio
+                    value="extremegain"
+                    onChange={() => handleRadioChange(key, "extremegain")}
+                    isChecked={value === "extremegain"}
+                    fontSize={{ base: "sm", md: "md", lg: "lg" }} // Adjust the sizes based on your designfontSize={{ base: "sm", md: "md", lg: "lg" }}  // Adjust the sizes based on your design
+                    mr={{ base: "3%", md: "3%", lg: "3%" }}
+                  >
+                    Екстремно качване на тегло
+                  </Radio>
+                </SimpleGrid>
+              </Stack>
+            ) : (
+              <Input
+                type="radio"
+                id={value}
+                name="goal"
+                value={value}
+                onChange={(e) => handleInputChange(e)}
+              />
+            )}
+          </label>
+        ))}
+        <Button type="submit" onClick={handleSubmit}>
+          Изпрати
+        </Button>
+      </Box>
+    </Card>
+  );
 };
 
 export default UserPersonalData;
--- src/views/admin/weightStats/components/UserPreferencesForMealPlanForm.tsx
+++ src/views/admin/weightStats/components/UserPreferencesForMealPlanForm.tsx
@@ -1,6 +1,6 @@
 // UserPreferencesInput.tsx
-import React from 'react';
-import { Input, Button, Text, Flex, Box, SimpleGrid} from "@chakra-ui/react";
+import React from "react";
+import { Input, Button, Text, Flex, Box, SimpleGrid } from "@chakra-ui/react";
 import { UserPreferencesForMealPlan } from "../../../../types/weightStats";
 import Card from "components/card/Card";
 
@@ -9,10 +9,13 @@ interface UserPreferencesInputProps {
   handleInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   generatePlan: () => void;
 }
-const fieldName: string[] = ["калории","протеин","мазнини","въглехидрати"];
+const fieldName: string[] = ["калории", "протеин", "мазнини", "въглехидрати"];
 
-const UserPreferencesForMealPlanForm: React.FC<UserPreferencesInputProps> = ({ userPreferences, handleInputChange, generatePlan }) => {
-
+const UserPreferencesForMealPlanForm: React.FC<UserPreferencesInputProps> = ({
+  userPreferences,
+  handleInputChange,
+  generatePlan
+}) => {
   const isUserDataValid = () => {
     // Validation logic goes here
     // Example: Numerical values should be greater than zero
@@ -25,63 +28,73 @@ const UserPreferencesForMealPlanForm: React.FC<UserPreferencesInputProps> = ({ u
   };
 
   const handleSubmit = () => {
-      if (isUserDataValid()) {
-        generatePlan();
-      } else {
+    if (isUserDataValid()) {
+      generatePlan();
+    } else {
       // Handle invalid data, you can show an error message or take any other appropriate action
-          alert('Please provide valid values for all fields before submitting.');
+      alert("Please provide valid values for all fields before submitting.");
       // Or you can use a state to manage and display an error message in your UI
       // setError('Please provide valid values for all fields before submitting.');
-      }
+    }
   };
-  
+
   return (
     <Card>
-      <SimpleGrid columns={{ base: 1, md: 2 }} gap={{ base: '10px', md: '20px' }}>
+      <SimpleGrid
+        columns={{ base: 1, md: 2 }}
+        gap={{ base: "10px", md: "20px" }}
+      >
         {Object.entries(userPreferences).map(([key, value], index) => (
           <Box key={key}>
             <Flex justify="center" pt="5px" w="100%" mt="5px">
               <Text fontSize="2xl">
-                {fieldName[index].charAt(0).toUpperCase() + fieldName[index].slice(1)}:
+                {fieldName[index].charAt(0).toUpperCase() +
+                  fieldName[index].slice(1)}
+                :
               </Text>
             </Flex>
             <Flex>
-              {value !== 0 ?
-                  (<Input
-                    isRequired={true}
-                    variant='auth'
-                    fontSize={{ base: 'sm', md: 'md' }}
-                    ms={{ base: '0px', md: '0px' }}
-                    placeholder={"Въведете " + fieldName[index]}
-                    value={value || ''}
-                    mt={{ base: '1%', md: '2%' }}
-                    fontWeight='500'
-                    size='lg'
-                    type="number"
-                    name={key}
-                    onChange={handleInputChange}
-                  />)     
-                :
-                  (<Input
-                    isRequired={true}
-                    variant='auth'
-                    fontSize={{ base: 'sm', md: 'md' }}
-                    ms={{ base: '0px', md: '0px' }}
-                    placeholder={"Въведете " + fieldName[index]}
-                    value={''}
-                    mt={{ base: '1%', md: '2%' }}
-                    fontWeight='500'
-                    size='lg'
-                    type="number"
-                    name={key}
-                    onChange={handleInputChange}
-                  />)
-              }
+              {value !== 0 ? (
+                <Input
+                  isRequired={true}
+                  variant="auth"
+                  fontSize={{ base: "sm", md: "md" }}
+                  ms={{ base: "0px", md: "0px" }}
+                  placeholder={"Въведете " + fieldName[index]}
+                  value={value || ""}
+                  mt={{ base: "1%", md: "2%" }}
+                  fontWeight="500"
+                  size="lg"
+                  type="number"
+                  name={key}
+                  onChange={handleInputChange}
+                />
+              ) : (
+                <Input
+                  isRequired={true}
+                  variant="auth"
+                  fontSize={{ base: "sm", md: "md" }}
+                  ms={{ base: "0px", md: "0px" }}
+                  placeholder={"Въведете " + fieldName[index]}
+                  value={""}
+                  mt={{ base: "1%", md: "2%" }}
+                  fontWeight="500"
+                  size="lg"
+                  type="number"
+                  name={key}
+                  onChange={handleInputChange}
+                />
+              )}
             </Flex>
           </Box>
         ))}
       </SimpleGrid>
-      <Button onClick={handleSubmit} mt={{ base: '10%', lg: '2%' }} mb={{ base: '15%', lg: '0%' }} minH="15%">
+      <Button
+        onClick={handleSubmit}
+        mt={{ base: "10%", lg: "2%" }}
+        mb={{ base: "15%", lg: "0%" }}
+        minH="15%"
+      >
         Създайте хранителен план
       </Button>
     </Card>
