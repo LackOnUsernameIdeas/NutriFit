73980de156470a3602f2afab73bd744f8127b12f
LackOnUsernameIdeas <kaloyan.kostadinov2007@abv.bg>
2024-02-03 19:21:07 +0200

    added empty page

--- src/routes.tsx
+++ src/routes.tsx
@@ -15,6 +15,7 @@ import DataTables from "views/admin/dataTables";
 import WeightStats from "views/admin/weightStats";
 import MealPlanner from "views/admin/mealPlanner";
 import UserMeasurements from "views/userMeasurements";
+import Contact from "views/admin/contact";
 
 // Auth Imports
 import SignInCentered from "views/auth/signIn";
@@ -80,7 +81,7 @@ const routes = [
     hideInSidebar: true
   },
   {
-    name: "Вход",
+    name: "Регистрация",
     layout: "/auth",
     path: "/sign-up",
     icon: <Icon as={MdLock} width="20px" height="20px" color="inherit" />,
@@ -88,7 +89,7 @@ const routes = [
     hideInSidebar: true
   },
   {
-    name: "Вход",
+    name: "Забравена парола",
     layout: "/auth",
     path: "/forgot-password",
     icon: <Icon as={MdLock} width="20px" height="20px" color="inherit" />,
@@ -110,6 +111,14 @@ const routes = [
     icon: <Icon as={MdLock} width="20px" height="20px" color="inherit" />,
     component: UserMeasurements,
     hideInSidebar: true
+  },
+  {
+    name: "За контакт",
+    layout: "/admin",
+    path: "/contact",
+    icon: <Icon as={MdLock} width="20px" height="20px" color="inherit" />,
+    component: Contact,
+    hideInSidebar: false
   }
 ];
 
---
+++ src/views/admin/contact/index.tsx
@@ -0,0 +1,129 @@
+/*!
+  _   _  ___  ____  ___ ________  _   _   _   _ ___   
+ | | | |/ _ \|  _ \|_ _|__  / _ \| \ | | | | | |_ _| 
+ | |_| | | | | |_) || |  / / | | |  \| | | | | || | 
+ |  _  | |_| |  _ < | | / /| |_| | |\  | | |_| || |
+ |_| |_|\___/|_| \_\___/____\___/|_| \_|  \___/|___|
+                                                                                                                                                                                                                                                                                                                                       
+=========================================================
+* Horizon UI - v1.1.0
+=========================================================
+
+* Product Page: https://www.horizon-ui.com/
+* Copyright 2022 Horizon UI (https://www.horizon-ui.com/)
+
+* Designed and Coded by Simmmple
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+// Chakra imports
+import {
+  Avatar,
+  Button,
+  Box,
+  Flex,
+  FormLabel,
+  Icon,
+  Select,
+  SimpleGrid,
+  useColorMode,
+  useColorModeValue,
+  Text,
+  Link
+} from "@chakra-ui/react";
+// Assets
+import Bulgaria from "assets/img/dashboards/bulgaria.png";
+// Custom components
+import MiniStatistics from "components/card/MiniStatistics";
+import Card from "components/card/Card";
+import IconBox from "components/icons/IconBox";
+import {
+  MdAddTask,
+  MdAttachMoney,
+  MdBarChart,
+  MdFileCopy
+} from "react-icons/md";
+import {
+  getFirestore,
+  collection,
+  onSnapshot,
+  DocumentData,
+  getDocs
+} from "firebase/firestore";
+import FadeInWrapper from "components/wrapper/FadeInWrapper";
+import backgroundImageWhite from "../../../assets/img/layout/blurry-gradient-haikei-light.svg";
+import backgroundImageDark from "../../../assets/img/layout/blurry-gradient-haikei-dark.svg";
+import {
+  getTotalUsers,
+  getAverageWeightOfAllUsers,
+  getAverageCaloriesOfAllUsers,
+  getAverageProteinOfAllUsers,
+  getAverageCarbsOfAllUsers,
+  getAverageFatOfAllUsers,
+  getAverageBodyFatPercentageOfAllUsers
+} from "database/getMeanUsersData";
+import TotalSpent from "./components/TotalSpent";
+
+interface LinearGradientTextProps {
+  text: any;
+  gradient: string;
+  fontSize?: string;
+  fontFamily?: string;
+  mr?: string;
+}
+
+interface TimestampedObject {
+  date: string;
+  weight?: number;
+  calories?: number;
+  protein?: number;
+  carbs?: number;
+  fat?: number;
+  bodyFatPercentage?: number;
+}
+
+const LinearGradientText: React.FC<LinearGradientTextProps> = ({
+  text,
+  gradient,
+  fontSize,
+  fontFamily,
+  mr
+}) => (
+  <Text
+    as="span"
+    fontSize={fontSize}
+    fontFamily={fontFamily}
+    fontWeight="bold"
+    mr={mr}
+    style={{
+      backgroundImage: gradient,
+      WebkitBackgroundClip: "text",
+      color: "transparent"
+    }}
+  >
+    {text}
+  </Text>
+);
+
+export default function UserReports() {
+  // Chakra Color Mode
+  const { colorMode } = useColorMode();
+  const backgroundImage =
+    colorMode === "light" ? backgroundImageWhite : backgroundImageDark;
+  const brandColor = useColorModeValue("brand.500", "white");
+  const gradientLight = "linear-gradient(90deg, #422afb 0%, #715ffa 100%)";
+  const gradientDark = "linear-gradient(90deg, #715ffa 0%, #422afb 100%)";
+  const gradientNutri = useColorModeValue(gradientLight, gradientDark);
+  const gradientFit = useColorModeValue(gradientDark, gradientLight);
+  const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
+  const fontWeight = useColorModeValue("500", "100");
+  const bgHover = useColorModeValue("secondaryGray.200", "secondaryGray.900");
+  const bgFocus = { bg: "brand.200" };
+  const [loading, setLoading] = React.useState(true);
+
+  return <Box></Box>;
+}
