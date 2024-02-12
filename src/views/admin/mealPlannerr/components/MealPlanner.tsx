05daee38068e0db9612bd98b414034ca344c30d9
LackOnUsernameIdeas <kaloyan.kostadinov2007@abv.bg>
2024-02-12 17:32:49 +0200

    Update MealPlanner.tsx

--- src/views/admin/mealPlannerr/components/MealPlanner.tsx
+++ src/views/admin/mealPlannerr/components/MealPlanner.tsx
@@ -125,7 +125,7 @@ export default function MealPlanner(props: {
             Authorization:
               "Bearer sk-Ja1Nh0rAuf46bcwBSDYLT3BlbkFJRgyok4zlqLLW16I8Ntmee"
           },
-          //Hosting: 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
+          //Hosting: sk-14yD7Jthy49wCjUxHFIIT3BlbkFJEs1Rgs3TpvI2c3dllWcII (without the second I)
           body: JSON.stringify({
             model: "gpt-3.5-turbo-0125",
             messages: [
