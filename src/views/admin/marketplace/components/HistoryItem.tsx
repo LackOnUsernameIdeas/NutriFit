47221a68bae2317bbc82e2ac3eeea937ebe2691c
Ivaylo Zdravkov <ivajlozdravkov77@gmail.com>
2024-02-21 22:42:06 +0200

    hard coded

--- src/views/admin/marketplace/components/HistoryItem.tsx
+++ src/views/admin/marketplace/components/HistoryItem.tsx
@@ -1,66 +1,149 @@
-import React from 'react';
-// Chakra imports
-import { Flex, Icon, Image, Text, useColorModeValue } from '@chakra-ui/react';
-// Custom components
-import Card from 'components/card/Card';
-// Assets
-import { FaEthereum } from 'react-icons/fa';
+import React, { useState } from "react";
+import { Flex, Icon, Image, Text, useColorModeValue } from "@chakra-ui/react";
+import { FaEthereum, FaAngleRight, FaAngleDown } from "react-icons/fa";
+import { useSpring, animated } from "react-spring";
+import Card from "components/card/Card";
 
 export default function NFT(props: {
-	image: string;
-	name: string;
-	author: string;
-	date: string;
-	price: string | number;
+  image: string;
+  name: string;
+  author: string;
+  date: string;
+  price: string | number;
 }) {
-	const { image, name, author, date, price } = props;
-	// Chakra Color Mode
-	const textColor = useColorModeValue('brands.900', 'white');
-	const bgItem = useColorModeValue(
-		{ bg: 'white', boxShadow: '0px 40px 58px -20px rgba(112, 144, 176, 0.12)' },
-		{ bg: 'navy.700', boxShadow: 'unset' }
-	);
-	const textColorDate = useColorModeValue('secondaryGray.600', 'white');
-	return (
-		<Card _hover={bgItem} bg='transparent' boxShadow='unset' px='24px' py='21px' transition='0.2s linear'>
-			<Flex direction={{ base: 'column' }} justify='center'>
-				<Flex position='relative' align='center'>
-					<Image src={image} w='66px' h='66px' borderRadius='20px' me='16px' />
-					<Flex
-						direction='column'
-						w={{ base: '70%', md: '100%' }}
-						me={{ base: '4px', md: '32px', xl: '10px', '3xl': '32px' }}>
-						<Text
-							color={textColor}
-							fontSize={{
-								base: 'md'
-							}}
-							mb='5px'
-							fontWeight='bold'
-							me='14px'>
-							{name}
-						</Text>
-						<Text
-							color='secondaryGray.600'
-							fontSize={{
-								base: 'sm'
-							}}
-							fontWeight='400'
-							me='14px'>
-							{author}
-						</Text>
-					</Flex>
-					<Flex me={{ base: '4px', md: '32px', xl: '10px', '3xl': '32px' }} align='center'>
-						<Icon as={FaEthereum} color={textColor} width='9px' me='7px' />
-						<Text fontWeight='700' fontSize='md' color={textColor}>
-							{price}
-						</Text>
-					</Flex>
-					<Text ms='auto' fontWeight='700' fontSize='sm' color={textColorDate}>
-						{date}
-					</Text>
-				</Flex>
-			</Flex>
-		</Card>
-	);
+  const { image, name, author, date, price } = props;
+  const [expanded, setExpanded] = useState(false);
+
+  const boxBg = useColorModeValue("secondaryGray.300", "navy.700");
+  const gradientLight = "linear-gradient(90deg, #422afb 0%, #715ffa 50%)";
+  const gradientDark = "linear-gradient(90deg, #715ffa 0%, #422afb 100%)";
+  const gradient = useColorModeValue(gradientLight, gradientDark);
+  const dropdownBoxBg = useColorModeValue("secondaryGray.300", "navy.700");
+  const dropdownActiveBoxBg = useColorModeValue("#d8dced", "#171F3D");
+  const textColor = useColorModeValue("brands.900", "white");
+  const bgItem = useColorModeValue(
+    { bg: "white", boxShadow: "0px 40px 58px -20px rgba(112, 144, 176, 0.12)" },
+    { bg: "navy.700", boxShadow: "unset" }
+  );
+  const textColorDate = useColorModeValue("secondaryGray.600", "white");
+
+  const [dropdownVisible, setDropdownVisible] = React.useState(false);
+  const [miniStatisticsVisible, setMiniStatisticsVisible] =
+    React.useState(false);
+  const [renderDropdown, setRenderDropdown] = React.useState(false);
+
+  const handleDropdownToggle = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+
+  const slideAnimationDrop = useSpring({
+    opacity: miniStatisticsVisible ? 1 : 0,
+    transform: `translateY(${dropdownVisible ? -50 : -90}px)`,
+    config: {
+      tension: dropdownVisible ? 170 : 200,
+      friction: dropdownVisible ? 12 : 20
+    }
+  });
+
+  React.useEffect(() => {
+    const handleRestSlidePositionChange = async () => {
+      if (dropdownVisible) {
+        setMiniStatisticsVisible(true);
+        setRenderDropdown(true);
+      } else {
+        setMiniStatisticsVisible(false);
+        await new Promise<void>((resolve) =>
+          setTimeout(() => {
+            resolve();
+            setRenderDropdown(false);
+          }, 150)
+        );
+      }
+    };
+
+    handleRestSlidePositionChange();
+  }, [dropdownVisible]);
+  return (
+    <Card
+      _hover={bgItem}
+      bg="transparent"
+      boxShadow="unset"
+      px="24px"
+      py="21px"
+      transition="0.2s linear"
+      onClick={handleDropdownToggle}
+      cursor="pointer"
+    >
+      <Flex direction={{ base: "column" }} justify="center">
+        <Flex position="relative" align="center" zIndex="1">
+          <Image src={image} w="66px" h="66px" borderRadius="20px" me="16px" />
+          <Flex
+            direction="column"
+            w={{ base: "70%", md: "100%" }}
+            me={{ base: "4px", md: "32px", xl: "10px", "3xl": "32px" }}
+          >
+            {expanded ? (
+              <Icon
+                as={FaAngleDown}
+                ms="auto"
+                fontSize="lg"
+                color={textColorDate}
+              />
+            ) : (
+              <Icon
+                as={FaAngleRight}
+                ms="auto"
+                fontSize="lg"
+                color={textColorDate}
+              />
+            )}
+            <Text
+              color={textColor}
+              fontSize={{
+                base: "md"
+              }}
+              mb="5px"
+              fontWeight="bold"
+              me="14px"
+            >
+              {name}
+            </Text>
+            <Text
+              color="secondaryGray.600"
+              fontSize={{
+                base: "sm"
+              }}
+              fontWeight="400"
+              me="14px"
+            >
+              {author}
+            </Text>
+          </Flex>
+          <Flex
+            me={{ base: "4px", md: "32px", xl: "10px", "3xl": "32px" }}
+            align="center"
+          >
+            <Icon as={FaEthereum} color={textColor} width="9px" me="7px" />
+            <Text fontWeight="700" fontSize="md" color={textColor}>
+              {price}
+            </Text>
+          </Flex>
+        </Flex>
+        {renderDropdown && (
+          <animated.div style={{ ...slideAnimationDrop, position: "relative" }}>
+            <Card
+              bg="transparent"
+              minH={{ base: "800px", md: "300px", xl: "180px" }}
+            >
+              <Flex direction="column" mt="50px">
+                <Text fontWeight="700" fontSize="2xl" color="purple">
+                  testing
+                </Text>
+              </Flex>
+            </Card>
+          </animated.div>
+        )}
+      </Flex>
+    </Card>
+  );
 }
--- src/views/admin/topMeals/index.tsx
+++ src/views/admin/topMeals/index.tsx
@@ -19,19 +19,14 @@ import {
 // Assets
 import FadeInWrapper from "components/wrapper/FadeInWrapper";
 import { orderMealsByFrequency } from "database/getAdditionalUserData";
-
+import { FaAngleDown, FaAngleUp } from "react-icons/fa";
 // Custom components
 import Loading from "views/admin/weightStats/components/Loading";
 import HistoryItem from "views/admin/marketplace/components/HistoryItem";
 import Card from "components/card/Card";
-
-// Assets
-import Nft1 from "assets/img/nfts/Nft1.png";
-import Nft2 from "assets/img/nfts/Nft2.png";
-import Nft3 from "assets/img/nfts/Nft3.png";
-import Nft4 from "assets/img/nfts/Nft4.png";
-import Nft5 from "assets/img/nfts/Nft5.png";
-import Nft6 from "assets/img/nfts/Nft6.png";
+import { useSpring, animated } from "react-spring";
+import { ColumnAvaragesChart } from "components/charts/BarCharts";
+import { LineAvaragesChart } from "components/charts/LineCharts";
 
 interface Meal {
   name: string;
@@ -42,19 +37,117 @@ interface Meal {
 export default function TopMeals() {
   // Chakra Color Mode
   const { colorMode } = useColorMode();
-  const [loading, setLoading] = React.useState(true);
-  const [allMeals, setAllMeals] = React.useState<Meal[]>([]);
+  const chartsColor = useColorModeValue("brand.500", "white");
+  const [loading, setLoading] = React.useState(false);
+  const boxBg = useColorModeValue("secondaryGray.300", "navy.700");
+  const gradientLight = "linear-gradient(90deg, #422afb 0%, #715ffa 50%)";
+  const gradientDark = "linear-gradient(90deg, #715ffa 0%, #422afb 100%)";
+  const gradient = useColorModeValue(gradientLight, gradientDark);
+  const dropdownBoxBg = useColorModeValue("secondaryGray.300", "navy.700");
+  const dropdownActiveBoxBg = useColorModeValue("#d8dced", "#171F3D");
+  const [allMeals, setAllMeals] = React.useState<Meal[]>([
+    {
+      name: "Tova",
+      count: 2,
+      image: "https://i.ytimg.com/vi/ej-8AxMAcAQ/maxresdefault.jpg"
+    },
+    {
+      name: "Onuy",
+      count: 1,
+      image: "https://gradcontent.com/lib/400x296/quinoa-spinach.webp"
+    },
+    {
+      name: "Tuyto",
+      count: 1,
+      image: "https://gradcontent.com/lib/400x296/zucchini-soup.webp"
+    },
+    {
+      name: "Tova",
+      count: 2,
+      image: "https://i.ytimg.com/vi/ej-8AxMAcAQ/maxresdefault.jpg"
+    },
+    {
+      name: "Onuy",
+      count: 1,
+      image: "https://gradcontent.com/lib/400x296/quinoa-spinach.webp"
+    },
+    {
+      name: "Tuyto",
+      count: 1,
+      image: "https://gradcontent.com/lib/400x296/zucchini-soup.webp"
+    },
+    {
+      name: "Tova",
+      count: 2,
+      image: "https://i.ytimg.com/vi/ej-8AxMAcAQ/maxresdefault.jpg"
+    },
+    {
+      name: "Onuy",
+      count: 1,
+      image: "https://gradcontent.com/lib/400x296/quinoa-spinach.webp"
+    },
+    {
+      name: "Tuyto",
+      count: 1,
+      image: "https://gradcontent.com/lib/400x296/zucchini-soup.webp"
+    }
+  ]);
   const textColor = useColorModeValue("secondaryGray.900", "white");
   const textColorBrand = useColorModeValue("brand.500", "white");
 
+  // React.useEffect(() => {
+  //   setLoading(true);
+  //   orderMealsByFrequency().then((sortedMeals) => {
+  //     console.log("Sorted meals by frequency:", sortedMeals);
+  //     setAllMeals(sortedMeals);
+  //     setLoading(false);
+  //   });
+  // }, []);
+
+  const [dropdownVisible, setDropdownVisible] = React.useState(false);
+  const [miniStatisticsVisible, setMiniStatisticsVisible] =
+    React.useState(false);
+  const [renderDropdown, setRenderDropdown] = React.useState(false);
+
+  const handleDropdownToggle = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+
+  const slideAnimationDrop = useSpring({
+    opacity: miniStatisticsVisible ? 1 : 0,
+    transform: `translateY(${dropdownVisible ? -50 : -90}px)`,
+    config: {
+      tension: dropdownVisible ? 170 : 200,
+      friction: dropdownVisible ? 12 : 20
+    }
+  });
+
+  const slideAnimation = useSpring({
+    transform: `translateY(${dropdownVisible ? -50 : 0}px)`,
+    config: {
+      tension: dropdownVisible ? 170 : 200,
+      friction: dropdownVisible ? 12 : 20
+    }
+  });
+
   React.useEffect(() => {
-    setLoading(true);
-    orderMealsByFrequency().then((sortedMeals) => {
-      console.log("Sorted meals by frequency:", sortedMeals);
-      setAllMeals(sortedMeals);
-      setLoading(false);
-    });
-  }, []);
+    const handleRestSlidePositionChange = async () => {
+      if (dropdownVisible) {
+        setMiniStatisticsVisible(true);
+        setRenderDropdown(true);
+      } else {
+        setMiniStatisticsVisible(false);
+        await new Promise<void>((resolve) =>
+          setTimeout(() => {
+            resolve();
+            setRenderDropdown(false);
+          }, 150)
+        );
+      }
+    };
+
+    handleRestSlidePositionChange();
+  }, [dropdownVisible]);
 
   return (
     <FadeInWrapper>
@@ -67,35 +160,110 @@ export default function TopMeals() {
           <Flex
             flexDirection="column"
             gridArea={{ xl: "1 / 3 / 2 / 4", "2xl": "1 / 2 / 2 / 3" }}
-            mb="20px"
           >
             <Card p="0px">
-              <Flex
-                align={{ sm: "flex-start", lg: "center" }}
-                justify="space-between"
-                w="100%"
-                px="22px"
-                py="18px"
+              <Card
+                onClick={handleDropdownToggle}
+                cursor="pointer"
+                zIndex="1"
+                position="relative"
+                bg={dropdownVisible ? dropdownBoxBg : dropdownBoxBg}
               >
-                <Text color={textColor} fontSize="xl" fontWeight="600">
-                  History
-                </Text>
-                <Button variant="action">See all</Button>
-              </Flex>
-
-              {/* Map over allMeals and render HistoryItem for each meal */}
-              {allMeals.map((meal, index) => (
-                <HistoryItem
-                  key={index}
-                  name={meal.name}
-                  // Set author, date, image, and price according to your data structure
-                  author={"Брой: " + meal.count.toString()}
-                  date="Date"
-                  image={meal.image} // You may want to update this based on the meal data
-                  price="Price"
-                />
-              ))}
+                <Flex
+                  align={{ sm: "flex-start", lg: "center" }}
+                  justify="space-between"
+                  w="100%"
+                >
+                  <Text
+                    color={textColor}
+                    fontSize="2xl"
+                    style={
+                      dropdownVisible
+                        ? {
+                            backgroundImage: gradient,
+                            WebkitBackgroundClip: "text",
+                            color: "transparent"
+                          }
+                        : {}
+                    }
+                    userSelect="none"
+                  >
+                    {dropdownVisible ? (
+                      <b>Най-често препоръчани храни от ChatGPT!</b>
+                    ) : (
+                      "Най-често препоръчани храни от ChatGPT!"
+                    )}
+                  </Text>
+                  <Icon
+                    as={dropdownVisible ? FaAngleUp : FaAngleDown}
+                    boxSize={6}
+                    color="linear-gradient(90deg, #422afb 0%, #715ffa 100%)"
+                  />
+                </Flex>
+              </Card>
+              {renderDropdown && (
+                <animated.div
+                  style={{ ...slideAnimationDrop, position: "relative" }}
+                >
+                  <Box mt="50px">
+                    {allMeals.map((meal, index) => (
+                      <HistoryItem
+                        key={index}
+                        name={meal.name}
+                        // Set author, date, image, and price according to your data structure
+                        author={"Брой: " + meal.count.toString()}
+                        date="Date"
+                        image={meal.image} // You may want to update this based on the meal data
+                        price="Price"
+                      />
+                    ))}
+                  </Box>
+                </animated.div>
+              )}
             </Card>
+            <animated.div style={{ ...slideAnimation, position: "relative" }}>
+              <SimpleGrid
+                columns={{ base: 1, md: 2, xl: 2 }}
+                gap="20px"
+                mt="20px"
+              >
+                <Card
+                  alignItems="center"
+                  flexDirection="column"
+                  minH={{ sm: "150px", md: "300px", lg: "300px" }}
+                  minW={{ sm: "150px", md: "200px", lg: "100%" }} // Adjusted minW for responsiveness
+                  maxH="400px"
+                  mt="40px"
+                >
+                  {/* BarChart component with adjusted width */}
+                  <LineAvaragesChart
+                    lineChartData={[4, 2]}
+                    lineChartData2={[2, 4]}
+                    lineChartLabels={["yes", "no"]}
+                    lineChartLabelName={"Мъже"}
+                    lineChartLabelName2={"Жени"}
+                    textColor={chartsColor}
+                  />
+                </Card>
+                <Card
+                  alignItems="center"
+                  flexDirection="column"
+                  minH={{ sm: "150px", md: "300px", lg: "300px" }}
+                  minW={{ sm: "150px", md: "200px", lg: "100%" }} // Adjusted minW for responsiveness
+                  maxH="400px"
+                  mt="40px"
+                >
+                  <ColumnAvaragesChart
+                    chartData={[4, 2]}
+                    chartData2={[2, 4]}
+                    chartLabels={["yes", "no"]}
+                    chartLabelName={"Мъже"}
+                    chartLabelName2={"Жени"}
+                    textColor={chartsColor}
+                  />
+                </Card>
+              </SimpleGrid>
+            </animated.div>
           </Flex>
         )}
       </Box>
