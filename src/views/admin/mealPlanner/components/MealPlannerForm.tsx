9b39f31dc6d34907c1a5910490c057e77f72bf99
LackOnUsernameIdeas <kaloyan.kostadinov2007@abv.bg>
2024-03-21 22:37:56 +0200

    Update MealPlannerForm.tsx

--- src/views/admin/mealPlanner/components/MealPlannerForm.tsx
+++ src/views/admin/mealPlanner/components/MealPlannerForm.tsx
@@ -495,13 +495,13 @@ export default function MealPlannerForm(props: {
           async function fetchImage(name: string): Promise<any> {
             try {
               let response = await fetch(
-                `https://customsearch.googleapis.com/customsearch/v1?key=AIzaSyBGskRKof9dkcoXtReamm4-h7UorF1G7yM&cx=10030740e88c842af&q=${encodeURIComponent(
+                `https://customsearch.googleapis.com/customsearch/v1?key=AIzaSyDqUez1TEmLSgZAvIaMkWfsq9rSm0kDjIw&cx=10030740e88c842af&q=${encodeURIComponent(
                   name
                 )}&searchType=image`
               );
               if (response.status === 429) {
                 let response = await fetch(
-                  `https://customsearch.googleapis.com/customsearch/v1?key=AIzaSyBpwC_IdPQ2u-16x_9QwoqJDu-zMhuFKxs&cx=258e213112b4b4492&q=${encodeURIComponent(
+                  `https://customsearch.googleapis.com/customsearch/v1?key=AIzaSyBQMvBehFDpwqhNc9_q-lIfPh8O2xdQ1Mc&cx=258e213112b4b4492&q=${encodeURIComponent(
                     name
                   )}&searchType=image`
                 );
